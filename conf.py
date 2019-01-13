317211bf2970a988ca716705d29c1dffbea07866
ciro spataro <cirospat@gmail.com>
2019-01-13 18:53:14 +0100

    Update conf.py

--- conf.py
+++ conf.py
@@ -40,8 +40,8 @@ def setup(app):
 """
 #latex_documents = []
 
-latex_logo = "static/logo.jpg"
-html_logo = "static/logo.jpg"
+latex_logo = "static/logo.JPG"
+html_logo = "static/logo.JPG"
 
 
 # Adding Custom CSS or JavaScript to a Sphinx Project: al seguente link ci sono esempi
